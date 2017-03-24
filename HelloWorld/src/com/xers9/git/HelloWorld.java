6759c45aecec33f3d2f02c5363c618cc815c4ddb
Kyudong Kim <xers989@gmail.com>
2017-03-24 17:41:37 +0900

    Comit

---
+++ HelloWorld/src/com/xers9/git/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.xers9.git;
+
+public class HelloWorld {
+    public HelloWorld() {
+        super();
+    }
+    
+    public void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
